f1f7f5cc9196a31dd5d6a62daf343b2124adc573
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2016-08-23 11:49:21 +0800

    加入webhook

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.get('/webhook', (req, res) => {
+    if (req.query['hub.mode'] === 'subscribe' &&
+        req.query['hub.verify_token'] === VALIDATION_TOKEN) {
+        console.log("Validating webhook");
+        res.status(200).send(req.query['hub.challenge']);
+    } else {
+        console.error("Failed validation. Make sure the validation tokens match.");
+        res.sendStatus(403);
+    }
+});
