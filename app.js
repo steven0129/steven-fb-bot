24ce5567dece252e1614d68405b70e1108e0e8f1
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2016-08-24 16:35:06 +0800

    加入圖片

--- app.js
+++ app.js
@@ -16,10 +16,8 @@ bot.on('message', (payload, reply) => {
     let text = payload.message.text;
     bot.getProfile(payload.sender.id, (err, profile) => {
         if (err) throw err
-
         reply({ text: profile.last_name + profile.first_name + ' 您好, 您剛剛傳送了一個訊息:' + text }, (err) => {
             if (err) throw err
-
             console.log(`Echoed back to ${profile.first_name} ${profile.last_name}: ${text}`)
         })
     })
