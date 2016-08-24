f8197bb64eececc4a5c55a13926a5f01437f662b
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2016-08-24 16:57:59 +0800

    加入圖片

--- app.js
+++ app.js
@@ -14,9 +14,18 @@ bot.on('error', (err) => {
 
 bot.on('message', (payload, reply) => {
     let text = payload.message.text;
+    
     bot.getProfile(payload.sender.id, (err, profile) => {
         if (err) throw err
-        reply({ text: profile.last_name + profile.first_name + ' 您好, 您剛剛傳送了一個訊息:' + text }, (err) => {
+        reply({ 
+            text: profile.last_name + profile.first_name + ' 您好, 您剛剛傳送了一個訊息:' + text,
+            attachment: {
+                type:"image",
+                payload:{
+                    url:"http://x.rce.tw/s/h3584935/messenger-bot-store.jpg"
+                }
+            }
+        }, (err) => {
             if (err) throw err
             console.log(`Echoed back to ${profile.first_name} ${profile.last_name}: ${text}`)
         })
